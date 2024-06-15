8fa64a3b99578bd2c5ad3ccaf4367f3d37ac1f25
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-15 16:39:33 +0200

    chore: Refactor main function to handle user input for cave selection

--- dragon/main.py
+++ dragon/main.py
@@ -1,9 +1,14 @@
+import random
+
 def main():
-    userInput= input("Enter a left or right: ")
-    if userInput == "left":
-        print("You are in the dragon room")
-    elif userInput == "right":
-        print("You are in the treasure room")
+    userInput: str = input("Which cave do you want to enter? (1 or 2)")
+    
+    number: int = random.randint(1, 2)
+    
+    if userInput == str(number):
+        print("You have entered the cave with the treasure!")
+    else:
+        print("You have entered the cave with the dragon!")
 
 
 if __name__ == "__main__":
