d5351a3f38e5b91f8f44cec8413ffad05805a6b1
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-17 15:46:20 +0200

    extract enter cave

--- dragon/main.py
+++ dragon/main.py
@@ -1,20 +1,13 @@
 import random
-import render
+import cave
+
 
 def main():
-    userInput: str = input("Which cave do you want to enter? (1 or 2)")
-    
-    number: int = random.randint(1, 2)
-    
-    print(enter_cave(userInput, number))
+    user_input = input("Which cave do you want to enter?")
 
-def enter_cave(userInput: str, number: int) -> str:
-    if userInput == str(number):
-        render.treasure()
-        return("You have entered the cave with the treasure!")
-    else:
-        render.dragon()
-        return("You have entered the cave with the dragon!")
+    random_number = str(random.randint(0, 2))
+
+    print(cave.enter(user_input, random_number))
 
 
 if __name__ == "__main__":
